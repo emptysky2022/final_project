c6d53bdfeb5d1cf9a3496284a371757f79430803
ByeongJunAn <abj5237@nate.com>
2023-02-08 04:41:16 +0900

    main...ing

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 "use strict";
 
-const hamburger = document.querySelector('.nav__hamburger');
-const menu = document.querySelector('.nav_menu');
-const mypage = document.querySelector('.nav_mypage');
+/*화면크기가 태블릿 이하일 때, 네비게이션에서 햄버거 누르면 메뉴 보이게 하는 기능 */
+const hamburger = document.querySelector("header .header_hamburger");
+const menu = document.querySelector("header .box2_2");
+const mypage = document.querySelector("header .box2_3");
+
+console.log(hamburger);
 
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('active');
