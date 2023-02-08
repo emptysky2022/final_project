a87a07a2aba8fd652f275a7deacb4a464e4bd895
StrHyunSoo <gustn995@gmail.com>
2023-02-09 00:42:30 +0900

    1. User 에 있는거 member로 변경 안되어있는 부분 변경 2. 1:N 관계 entity @ondelte 로 부모삭제시 자식 삭제되게 변경

--- project/src/main/java/com/campers/camfp/repository/CampCalanderRepository.java
+++ project/src/main/java/com/campers/camfp/repository/CampCalanderRepository.java
@@ -3,11 +3,11 @@ package com.campers.camfp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.campers.camfp.entity.CampCalander;
+import com.campers.camfp.entity.CampCalender;
 
 
 
-public interface CampCalanderRepository extends JpaRepository<CampCalander, Long> {
+public interface CampCalanderRepository extends JpaRepository<CampCalender, Long> {
 
 	
 }
