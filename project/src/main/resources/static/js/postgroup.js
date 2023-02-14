c1d731515ee623e7c3fbb45ea56771d215cef8b5
ByeongJunAn <abj5237@nate.com>
2023-02-15 01:40:45 +0900

    home

--- project/src/main/resources/static/js/postgroup.js
+++ project/src/main/resources/static/js/postgroup.js
@@ -17,53 +17,59 @@ hamburger.addEventListener('click', () => {
 /*input file타입의 이미지 파일만 올리게 하기 위해 사용*/
 function chk_file_type(obj) {
     var file_kind = obj.value.lastIndexOf('.');
-    var file_name = obj.value.substring(file_kind+1,obj.length);
+    var file_name = obj.value.substring(file_kind + 1, obj.length);
     var file_type = file_name.toLowerCase();
-   
-   
-   
-    var check_file_type=new Array();
 
-    check_file_type=['jpg','gif','png','jpeg','bmp'];
-   
-   
-   
-    if(check_file_type.indexOf(file_type)==-1){
-     alert('이미지 파일만 선택할 수 있습니다.');
-     var parent_Obj=obj.parentNode
-     var node=parent_Obj.replaceChild(obj.cloneNode(true),obj);
-     return false;
+
+
+    var check_file_type = new Array();
+
+    check_file_type = ['jpg', 'gif', 'png', 'jpeg', 'bmp'];
+
+
+
+    if (check_file_type.indexOf(file_type) == -1) {
+        alert('이미지 파일만 선택할 수 있습니다.');
+        var parent_Obj = obj.parentNode
+        var node = parent_Obj.replaceChild(obj.cloneNode(true), obj);
+        return false;
     }
-   }
-   
+}
+
 
 function toggleBtn1() {
-
-
-
-    // 토글 할 버튼 선택 (btn2)
-   
-    var btn2 = document.querySelector('.sec1');
- console.log(btn2)
-    
-
-
+    // 토글 할 버튼 선택 (.sec1) 
+    var sec1 = document.querySelector('.sec1');
+    console.log(sec1)
     // btn1 숨기기 (display: none)
-
-    if(btn2.style.display !== 'none') {
-
-btn2.style.display = 'none';
-
+    if (sec1.style.display !== 'none') {
+        sec1.style.display = 'none';
     }
-
     // btn` 보이기 (display: block)
-
     else {
-
-btn2.style.display = 'block';
-
+        sec1.style.display = 'block';
     }
-    
-    
+}
 
+
+function toggleBtn2() {
+    var sec2 = document.querySelector('.sec2');
+    console.log(sec2)
+    if (sec2.style.display !== 'none') {
+        sec2.style.display = 'none';
+    }
+    else {
+        sec2.style.display = 'block';
+    }
+}
+
+function toggleBtn3() {
+    var sec3 = document.querySelector('.sec3');
+    console.log(sec3)
+    if (sec3.style.display !== 'none') {
+        sec3.style.display = 'none';
+    }
+    else {
+        sec3.style.display = 'block';
+    }
 }
