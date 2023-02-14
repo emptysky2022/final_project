b587007a3274eca4d22e14254bd3f1c072763875
keduit <keduit@505-01>
2023-02-14 12:36:44 +0900

    1.파일삭제 재업로드

--- project/src/main/java/com/campers/camfp/repository/camp/CampQuerydsl.java
+++ project/src/main/java/com/campers/camfp/repository/camp/CampQuerydsl.java
@@ -12,14 +12,14 @@ public interface CampQuerydsl {
 	  * @param address 찾을 주소 ex) 경기도 , 강원도 , 서울 <br>
 	  * @return List<?> List WildCard
 	  */
-	public List<?> findByAddress(TableType table, String address);
+	//public List<?> findByAddress(TableType table, String address);
 	
 	 /** 
 	  * @param table 변환할 table 명 table <br>
 	  * @param address 찾을 주소 ex) 경기도 , 강원도 , 서울 <br>
 	  * @return List<?> List WildCard
 	  */
-	public List<?> findHeartRank(TableType table, int count);
+	//public List<?> findHeartRank(TableType table, int count);
 	
 	
 	 /** 
@@ -27,7 +27,7 @@ public interface CampQuerydsl {
 	  * @param address 찾을 주소 ex) 경기도 , 강원도 , 서울 <br>
 	  * @return List<?> List WildCard
 	  */
-	public List<?> findDataOfMember(TableType table, int count);
+	//public List<?> findDataOfMember(TableType table, int count);
 	
 }
 	
