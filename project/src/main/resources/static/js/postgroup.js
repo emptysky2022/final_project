41001946e6bbafa8930bc526866cffafd2c9eb53
ByeongJunAn <abj5237@nate.com>
2023-02-14 20:56:23 +0900

    참고용

--- project/src/main/resources/static/js/postgroup.js
+++ project/src/main/resources/static/js/postgroup.js
@@ -1,3 +1,19 @@
+"use strict";
+
+
+/*화면크기가 태블릿 이하일 때, 네비게이션에서 햄버거 누르면 메뉴 보이게 하는 기능 */
+const hamburger = document.querySelector("header .header_hamburger");
+const menu = document.querySelector("header .box2_2");
+const mypage = document.querySelector("header .box2_3");
+
+console.log(hamburger);
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    mypage.classList.toggle('active');
+});
+
+
 /*input file타입의 이미지 파일만 올리게 하기 위해 사용*/
 function chk_file_type(obj) {
     var file_kind = obj.value.lastIndexOf('.');
@@ -6,8 +22,8 @@ function chk_file_type(obj) {
    
    
    
-    var check_file_type=new Array();​
-   
+    var check_file_type=new Array();
+
     check_file_type=['jpg','gif','png','jpeg','bmp'];
    
    
@@ -20,4 +36,34 @@ function chk_file_type(obj) {
     }
    }
    
+
+function toggleBtn1() {
+
+
+
+    // 토글 할 버튼 선택 (btn2)
    
+    var btn2 = document.querySelector('.sec1');
+ console.log(btn2)
+    
+
+
+    // btn1 숨기기 (display: none)
+
+    if(btn2.style.display !== 'none') {
+
+btn2.style.display = 'none';
+
+    }
+
+    // btn` 보이기 (display: block)
+
+    else {
+
+btn2.style.display = 'block';
+
+    }
+    
+    
+
+}
