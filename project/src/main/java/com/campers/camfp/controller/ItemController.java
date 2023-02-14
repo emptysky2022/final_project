071d1e7c3485fb1d6b53ed241c13df324491c091
emptysky2022 <100741315+emptysky2022@users.noreply.github.com>
2023-02-14 16:17:06 +0900

    Item 날아간거 재업로드 & html에 데이터 넣기 (#47)

--- project/src/main/java/com/campers/camfp/controller/ItemController.java
+++ project/src/main/java/com/campers/camfp/controller/ItemController.java
@@ -1,7 +1,17 @@
 package com.campers.camfp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.campers.camfp.dto.item.ItemDTO;
+import com.campers.camfp.service.item.ItemService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -9,7 +19,20 @@ import lombok.extern.log4j.Log4j2;
 @Controller
 @Log4j2
 @RequiredArgsConstructor
-@RequestMapping("/items/")
+@RequestMapping("/item")
 public class ItemController {
+	
+	private final ItemService itemService;
 
+	@GetMapping("/list")
+	public void itemList(Model model) {
+		List<ItemDTO> itemLists = new ArrayList<>();
+		for(Long i = 2L; i <= 10L; i++) {
+			itemLists.add(itemService.getOne(i));
+		}
+		
+		log.info(itemLists);
+		model.addAttribute("result", itemLists);
+	}
+	
 }
