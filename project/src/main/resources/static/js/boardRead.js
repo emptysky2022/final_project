ba47806ab366ee807c9480bf07434262eb4a1d2a
lagoon284 <lagoon284@naver.com>
2023-02-15 17:37:45 +0900

    커밋.

--- project/src/main/resources/static/js/boardRead.js
+++ project/src/main/resources/static/js/boardRead.js
@@ -1,3 +1,15 @@
-/**
- * 
- */
+$(function(){
+	function loadJsonData(){
+		let bno = new URLSearchParams(window.location.search).get("bno");
+		let boardListRE = $("#boardListRE");
+		$.getJSON("/sample/read/" + bno, function(result){
+			console.log(result);
+			let str = "";
+			
+			if()
+			
+			boardListRE.html(str);
+			console.log("완")
+		})
+	}
+})
