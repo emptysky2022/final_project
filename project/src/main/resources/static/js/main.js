197f1dbf530074d8413a4b75955656b740a82a59
ByeongJunAn <abj5237@nate.com>
2023-02-22 21:01:33 +0900

    aa

--- project/src/main/resources/static/js/main.js
+++ project/src/main/resources/static/js/main.js
@@ -14,29 +14,69 @@ hamburger.addEventListener('click', () => {
 });
 
 
-/*swiper*/
+var swiper = new Swiper(".mySwiper", {
+  speed: 600,
+  parallax: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-  const progressCircle = document.querySelector(".autoplay-progress svg");
-    const progressContent = document.querySelector(".autoplay-progress span");
-    var swiper = new Swiper(".mySwiper", {
-      spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-      },
-      on: {
-        autoplayTimeLeft(s, time, progress) {
-          progressCircle.style.setProperty("--progress", 1 - progress);
-          progressContent.textContent = `${Math.ceil(time / 1000)}s`;
-        }
-      }
-    });
+
+
+//typeit 화면에 글씨써주는 효과
+document.addEventListener('DOMContentLoaded', ()=> {
+  new TypeIt('.ni_text')
+  .pause(1500)
+  .delete(11, {delay: 1000})
+  .type('이번엔 어디를 가볼까?')
+  .pause(1500)
+  .delete(12, {delay: 1000})
+  .type('어떤 캠핑용품이 필요하지?')
+  .pause(1500)
+  .delete(14, {delay: 1000})
+  .pause(1500)
+  .type('고민 하지 마세요!')
+  
+  .go()
+  .loop()
+})
+
+
+
+//scrollOut 스크롤 내릴때 효과
+// ScrollOut({
+  
+//   onShown: (element) => {
+//     new TypeIt(".scroll", {
+//       startDelay: 500,
+//       cursor: false,
+//     })
+//     .pause(2000)
+//     .go();
+//   },
+// });
+
+
+ScrollOut({
+  
+  onShown: (element) => {
+    new TypeIt(element.querySelector('.scroll'), {
+      startDelay: 500,
+      cursor: false,
+    })
+    .pause(2000)
+    .go();
+  },
+});
+
+
+
+//스크롤 내릴 때 속도를 다르게 줄 수 있다.
+
+var rellax = new Rellax('.rellax');
