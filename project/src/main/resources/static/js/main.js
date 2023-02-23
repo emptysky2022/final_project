10ac630812e6d39093989098f924b06368bb2d16
ByeongJunAn <abj5237@nate.com>
2023-02-23 20:52:45 +0900

    .....

--- project/src/main/resources/static/js/main.js
+++ project/src/main/resources/static/js/main.js
@@ -3,23 +3,34 @@
 
 
 //typeit 화면에 글씨써주는 효과
-document.addEventListener('DOMContentLoaded', ()=> {
-  new TypeIt('.ni_text')
-  .pause(1500)
-  .delete(11, {delay: 1000})
-  .type('이번엔 어디를 가볼까?')
-  .pause(1500)
-  .delete(12, {delay: 1000})
-  .type('어떤 캠핑용품이 필요하지?')
-  .pause(1500)
-  .delete(14, {delay: 1000})
-  .pause(1500)
-  .type('Campers와 함께')
+new TypeIt('.ni_text', {
+  strings: ['Hello', 'World', 'TypeIt'], // 타이핑할 문자열 배열
+  speed: 100, // 타이핑 속도
+  loop: true, // 루프 옵션
+  cursorChar:''
+}).go(); 
+
+
+
+
+//typeit 화면에 글씨써주는 효과
+// document.addEventListener('DOMContentLoaded', ()=> {
+//   new TypeIt('.ni_text')
+//   .pause(1500)
+//   .delete(11, {delay: 1000})
+//   .type('이번엔 어디를 가볼까?')
+//   .pause(1500)
+//   .delete(12, {delay: 1000})
+//   .type('어떤 캠핑용품이 필요하지?')
+//   .pause(1500)
+//   .delete(14, {delay: 1000})
+//   .pause(1500)
+//   .type('Campers와 함께')
   
-  .go() 
+//   .go() 
   
  
-})
+// })
 
 
 
