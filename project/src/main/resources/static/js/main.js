5f9601a59c559cf703c26b464260752042730de1
ByeongJunAn <abj5237@nate.com>
2023-02-23 18:05:41 +0900

    임시커밋

--- project/src/main/resources/static/js/main.js
+++ project/src/main/resources/static/js/main.js
@@ -1,33 +1,6 @@
 "use strict";
 
 
-/*화면크기가 태블릿 이하일 때, 네비게이션에서 햄버거 누르면 메뉴 보이게 하는 기능 */
-const hamburger = document.querySelector("header .header_hamburger");
-const menu = document.querySelector("header .header_menu");
-const mypage = document.querySelector("header .header_mypage");
-
-console.log(hamburger);
-
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    mypage.classList.toggle('active');
-});
-
-
-var swiper = new Swiper(".mySwiper", {
-  speed: 600,
-  parallax: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
-
 
 //typeit 화면에 글씨써주는 효과
 document.addEventListener('DOMContentLoaded', ()=> {
