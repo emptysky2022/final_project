aee67e8ba8b55b6d6f9619220384d9a8a05735a4
emptysky2022 <100741315+emptysky2022@users.noreply.github.com>
2023-02-27 01:54:02 +0900

    장바구니 추가/수정/삭제 (#91)

--- project/src/main/java/com/campers/camfp/entity/item/Item.java
+++ project/src/main/java/com/campers/camfp/entity/item/Item.java
@@ -1,5 +1,8 @@
 package com.campers.camfp.entity.item;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,6 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.campers.camfp.entity.member.Member;
@@ -66,6 +70,9 @@ public class Item {
 	private double star;			//별점 평균
 	
 	private int heart;				//좋아요, 찜 수
+	
+	@OneToMany(mappedBy = "item", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<ItemReview> itemReviews = new ArrayList<>();
 	
 	public void increseCount() {
 		this.count++;
--- project/src/test/java/com/campers/camfp/service/ItemAPITests.java
+++ project/src/test/java/com/campers/camfp/service/ItemAPITests.java
@@ -52,7 +52,7 @@ public class ItemAPITests {
         		throw new RuntimeException("검색어 인코딩 실패",e);
         	}
         	
-	        String apiURL = "https://openapi.naver.com/v1/search/shop?query=" + text + "&display=100&start=1+"+i*100;    // JSON 결과
+	        String apiURL = "https://openapi.naver.com/v1/search/shop?query=" + text + "&display=100&start=1+"+i*10;    // JSON 결과
 	        Map<String, String> requestHeaders = new HashMap<>();
 	        requestHeaders.put("X-Naver-Client-Id", clientId);
 	        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
