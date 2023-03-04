8d01bcfce3cad2057ffd6f3dd127010ba98c1e95
StrHyunSoo <gustn995@gmail.com>
2023-03-04 23:54:22 +0900

    1. camp 최종수정 (#124)
    Co-authored-by: keduit <keduit@505-01>
    Co-authored-by: emptysky2022 <100741315+emptysky2022@users.noreply.github.com>

--- project/src/main/java/com/campers/camfp/controller/CampRestController.java
+++ project/src/main/java/com/campers/camfp/controller/CampRestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -104,6 +105,7 @@ public class CampRestController {
 
 	}
 
+	@Secured("ROLE_MEMBER")
 	@PostMapping("calendar/register")
 	public ResponseEntity<String> calendarRegister(@RequestBody CampCalenderDTO dto,
 			@AuthenticationPrincipal PrincipalDetails principalDetails) {
